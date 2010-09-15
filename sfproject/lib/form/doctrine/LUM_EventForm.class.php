087c47344790bcee7ccd32bb16b412dc26b3bb40
Tristan RIVOALLAN <tristan@cafe.com>
2010-09-16 01:49:35 +0200

    [model] Generated ORM classes.

---
+++ sfproject/lib/form/doctrine/LUM_EventForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * LUM_Event form.
+ *
+ * @package    musiques-incongrues
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class LUM_EventForm extends BaseLUM_EventForm
+{
+  public function configure()
+  {
+  }
+}
