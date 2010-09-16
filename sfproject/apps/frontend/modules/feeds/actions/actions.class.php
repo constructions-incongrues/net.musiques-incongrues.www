dc3a895afb5493f4cfeb0f0e0a166b54049936f8
Tristan RIVOALLAN <tristan@cafe.com>
2010-09-16 06:27:26 +0200

    [feeds] Implemented events RSS feed.

--- sfproject/apps/frontend/modules/feeds/actions/actions.class.php
+++ sfproject/apps/frontend/modules/feeds/actions/actions.class.php
@@ -41,7 +41,6 @@ class feedsActions extends sfActions
         $feed->setDateModified(time());
         foreach ($mixes as $mix)
         {
-
             $entry = $entry = $feed->createEntry();
             $entry->setTitle($mix['Discussion']['name']);
             // TODO : add slug
@@ -56,6 +55,61 @@ class feedsActions extends sfActions
                 $entry->addAuthor(array('name' => $mix['labelname']));
             }
             $entry->setEnclosure(array('uri' => $mix['downloadlink'], 'type' => 'audio/mpeg', 'length' => 666));
+            $feed->addEntry($entry);
+        }
+
+        // We don't want those
+        $this->setLayout(false);
+        sfConfig::set('sf_web_debug', false);
+
+        // Pass data to view
+        $this->feed = $feed;
+
+        // Configure response
+        $this->getResponse()->setContentType('application/rss+xml');
+
+        // Select template
+        return sfView::SUCCESS;
+    }
+
+    /**
+     * Generates RSS feeds of latest events.
+     *
+     * @param sfWebRequest $request
+     */
+    public function executeEvents(sfWebRequest $request)
+    {
+        // Fetch latest events
+        // TODO : refactor in model
+        $q = Doctrine_Query::create()
+            ->select('d.name, d.firstcommentid, e.date')
+            ->from('LUM_Event e')
+            ->innerJoin('e.Discussion d')
+            ->orderBy('d.DateCreated desc')
+            ->limit(50);
+        $events = $q->execute(null, Doctrine_Core::HYDRATE_ARRAY);
+        $q->free();
+
+        // Build
+        require_once 'Zend/Loader.php';
+        Zend_Loader::loadClass('Zend_Feed_Writer_Feed');
+        $feed = new Zend_Feed_Writer_Feed();
+        $feed->setTitle('Le calendrier des événements incongrus');
+        $feed->setLink('http://www.musiques-incongrues.net/forum/events');
+        $feed->setFeedLink('http://www.musiques-incongrues.net/forum/s/feeds/events', 'RSS');
+        $feed->setDescription("L'agenda collaboratif du forum des musiques incongrues");
+        $feed->setDateModified(time());
+        foreach ($events as $event)
+        {
+            $entry = $entry = $feed->createEntry();
+            $entry->setTitle($event['Discussion']['name']);
+            // TODO : add slug
+            $entry->setLink('http://www.musiques-incongrues.net/forum/discussion/'.$event['Discussion']['discussionid']);
+            // TODO : Make a better joined query
+            $comment = Doctrine_Core::getTable('LUM_Comment')->findOneByCommentid($event['Discussion']['firstcommentid'], Doctrine_Core::HYDRATE_ARRAY);
+            // TODO : parse BBCode
+            $entry->setDescription($comment['body']);
+            $entry->setContent($comment['body']);
             $feed->addEntry($entry);
         }
 
