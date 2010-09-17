4b0dca37918d7f2e5147a9629608a27bf160695a
Tristan RIVOALLAN <tristan@cafe.com>
2010-09-17 12:38:43 +0200

    [discussions] symfony discussion module stub.

---
+++ sfproject/apps/frontend/modules/discussion/actions/actions.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * discussion actions.
+ *
+ * @package    musiques-incongrues
+ * @subpackage discussion
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class discussionActions extends sfActions
+{
+ /**
+  * Displays discussions list.
+  *
+  * @param sfWebRequest $request A request object
+  */
+  public function executeList(sfWebRequest $request)
+  {
+  }
+}
