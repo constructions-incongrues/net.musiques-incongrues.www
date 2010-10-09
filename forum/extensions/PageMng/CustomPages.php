1e6d9492b8332d8d2847bc85f4cd4032fbb043ee
Tristan RIVOALLAN <tristan@cafe.com>
2010-10-09 11:55:01 +0200

    [pages] Fetched new page from server.

---
+++ forum/extensions/PageMng/CustomPages.php
@@ -0,0 +1,234 @@
+<?php return array(0 => array('tab' => 'Events', 'page' => 'events', 'url' => '/forum/events/', 'html' => 0, 'custom' => 1, 'attribs' => 'class="Eyes"', 'roles' => array(0 => 0, 1 => 4, 2 => 7, 3 => 8, 4 => 9, 5 => 10, 6 => 5, 7 => 2, 8 => 3, 9 => 6), 'hidden' => ''), 1 => array('tab' => 'Oeil', 'page' => 'Oeil', 'url' => '/forum/oeil/', 'html' => 0, 'custom' => 1, 'attribs' => 'class="Eyes"', 'roles' => array(0 => 0, 1 => 4, 2 => 7, 3 => 8, 4 => 9, 5 => 10, 6 => 5, 7 => 2, 8 => 3, 9 => 6), 'hidden' => ''), 2 => array('tab' => 'Releases', 'page' => 'releases', 'url' => '/forum/releases/', 'html' => 0, 'custom' => 1, 'attribs' => 'class="Eyes"', 'roles' => array(0 => 0, 1 => 4, 2 => 7, 3 => 8, 4 => 9, 5 => 10, 6 => 5, 7 => 2, 8 => 3, 9 => 6), 'hidden' => ''), 3 => array('tab' => 'TVi', 'page' => 'tvi', 'url' => 'http://www.tele-incongrue.net/', 'html' => 0, 'custom' => 1, 'attribs' => 'class="Eyes"', 'roles' => array(0 => 0, 1 => 4, 2 => 7, 3 => 8, 4 => 9, 5 => 10, 6 => 5, 7 => 2, 8 => 3, 9 => 6), 'hidden' => ''), 4 => array('tab' => 'Discussions', 'page' => 'discussions', 'url' => '/forum/discussions/', 'html' => 0, 'custom' => 0, 'attribs' => 'class="Pink"', 'roles' => array(0 => 0, 1 => 4, 2 => 7, 3 => 8, 4 => 9, 5 => 10, 6 => 5, 7 => 2, 8 => 3, 9 => 6), 'hidden' => ''), 5 => array('tab' => 'Categories', 'page' => 'categories', 'url' => '/forum/categories/', 'html' => 0, 'custom' => 0, 'attribs' => 'class="Pink"', 'roles' => array(0 => 0, 1 => 4, 2 => 7, 3 => 8, 4 => 9, 5 => 10, 6 => 5, 7 => 2, 8 => 3, 9 => 6), 'hidden' => ''), 6 => array('tab' => 'Search', 'page' => 'search', 'url' => '/forum/search/', 'html' => 0, 'custom' => 0, 'attribs' => 'class="Pink"', 'roles' => array(0 => 0, 1 => 4, 2 => 7, 3 => 8, 4 => 9, 5 => 10, 6 => 5, 7 => 2, 8 => 3, 9 => 6), 'hidden' => ''), 7 => array('tab' => 'Settings', 'page' => 'settings', 'url' => '/forum/settings/', 'html' => 0, 'custom' => 0, 'attribs' => 'class="Pink"', 'roles' => array(0 => 0, 1 => 4, 2 => 7, 3 => 8, 4 => 9, 5 => 10, 6 => 5, 7 => 2, 8 => 3, 9 => 6), 'hidden' => ''), 8 => array('tab' => 'Dons', 'page' => 'dons', 'url' => 0, 'html' => '<div id="pourquoijedonnemesorganes">
+<h2 class="surtout donate-1">Pourquoi faire un don ? </h2>
+
+<p>
+Et oui, qu\'allons-nous faire de vos espèces sonnantes et trébuchantes ?
+</p>
+
+<ul>
+<li>Financer les coûts de location des différents serveurs (<a href="http://www.pastis-hosting.net">Pastis Hosting</a>) et logiciels qui permettent l\'hébergement de ce forum, de quelques projets affiliés (<a href="http://www.thisisradioclash.org/">This is Radioclash</a>, <a href="http://www.pardon-my-french.fr">Pardon My French</a> <a href="http://www.musiqueapproximative.net/">Musique Approximative</a>, <a href="http://www.daheardit-records.net/">Da ! Heard It Records</a>, etc) et d\'un certain nombre d\'autres sites (<a href="http://www.modelepuissance.com/">Modèle Puissance</a>, <a href="http://www.collectionrevue.com/">Collection Revue</a>, <a href="http://www.sidabitball.com">Sidabitball</a>, <a href="http://musikmekanikcirkus.org/">Musik Mekanik Cirkus</a>, etc)</li>
+
+<li>Aider à se concrétiser les projets qui prennent vie sur le forum (par l\'acquisition de noms de domaines, etc)</li>
+<li>Imprimer sur des autocollants (ou autres) les belles pièces que nous offrent les graphistes du forum, dans l\'espoir de promouvoir un peu plus nos activités</li>
+<li>Et d\'autres idées nous viendrons sûrement en cours de route...</li>
+</ul>
+
+<p>
+Tous ces coûts sont supportés actuellement par un petit nombre de personnes. Un petit coup de main financier ne fera jamais de mal et permettra de faire encore plus
+<br>
+À vot\' bon cœur !
+</p>
+
+<h2 class="surtout donate-2">Comment faire un don ? </h2>
+
+<p>
+En cliquant sur le bouton Paypal ci-dessous, en nous envoyant un chèque ou en nous faisant des bisous.
+</p><p>
+
+</p><div id="donate">
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input name="cmd" value="_s-xclick" type="hidden">
+<input name="hosted_button_id" value="WTVDE68FYETMC" type="hidden">
+<input value="Faire un don" name="submit" class="donate" alt="Faire un don" type="submit">
+</form>
+</div>
+
+<h2 class="surtout donate-1">Merci à :</h2>
+
+<ul id="les_donneurs_dorganes">
+<li><a href="http://www.musiques-incongrues.net/forum/account/283/">Al Crotal</a></li>
+
+<li><a href="http://www.grandbazaarbernardgrancher.com/">Bernard Grancher</a></li>
+<li><a href="http://www.sammystein.fr/">Chaton Pute</a></li>
+<li><a href="http://www.myspace.com/eatrabbit">Eat Rabbit</a></li>
+<li><a href="http://www.glafouk.com">Glafouk</a></li>
+<li><a href="http://www.hyperkut.com/">Hyperkut</a></li>
+<li>Le Dave</li>
+<li><a href="http://puyopuyo.lautre.net">Puyo Puyo</a></li>
+<li><a href="http://www.myspace.com/rachitikdata">Rachitik Data</a></li>
+<li><a href="http://www.musiques-incongrues.net/forum/account/197/">Schling</a></li>
+
+<li><a href="http://www.sidabitball.com/">Sidabitball</a></li>
+<li><a href="http://www.thibaulthuertas.com/">Super Promotion</a></li>
+<li><a href="http://www.ouiedire.net/">Valkiri</a></li>
+<li><a href="http://egotwister.com/">Yan</a>', 'custom' => 1, 'attribs' => 'class="dons"', 'roles' => array(0 => 0, 1 => 4, 2 => 7, 3 => 8, 4 => 9, 5 => 10, 6 => 5, 7 => 2, 8 => 3, 9 => 6), 'hidden' => ''), 9 => array('tab' => 'Faq', 'page' => 'faq', 'url' => 0, 'html' => '<div id="pourquoijedonnemesorganes"> 
+<h2 class="surtout donate-2">Comment se servir du forum ? </h2> 
+
+<ul>
+<li><a href="#1">Commenter poster une image ?</a></li>
+<li><a href="#2">Peut-on redimensionner une image ?</a></li>
+<li><a href="#3">Peut-on éditer une image?</a></li>
+<li><a href="#4">Comment poster une vidéo ?</a></li>
+<li><a href="#5">Comment fonctionne la radio ?</a></li>
+<li><a href="#6">Qu\'est-ce que le whisper ?</a></li>
+<li><a href="#7">Peut on citer le message de quelqu\'un d\'autre pour lui répondre</a></li>
+<li><a href="#8">Comment écrit on gros ou petit ?</a></li>
+<li><a href="#9">Que signifient les petits carrés avec B I U etc ? </a></li>
+<li><a href="#10">Comment proposer un événement ?</a></li>
+<li><a href="#11">Qu\'est-ce que ŒIL et INTROSPECTION ?</a></li>
+<li><a href="#12">Qu\'est ce que RELEASES ?</a></li>
+<li><a href="#13">Comment proposer UNE RELEASE/UN MIX ?</a></li>
+<li><a href="#14">Qu\'est-ce que TVi ?</a></li>
+<li><a href="#15">Oui mais concrètement TVi c\'est quoi ?</a></li>
+<li><a href="#16">Comment peut on chercher un topic, un commentaire, un membre ?</a></li>
+<li><a href="#17">Peut on faire un don ?</a></li>
+<li><a href="#18">Qu\'est-ce qu\'un troll ?</a></li>
+<li><a href="19">Qu\'est-ce qu\'un topic ?</a></li>
+<li><a href="20">Comment puis-je mettre mon topic dans Et Surtout?</a></li>
+<li><a href="21">Comment nous contacter ?</a></li>
+<li><a href="22">Pourquoi tout ce qui est à faire n\'est pas fait en 5 minutes ?</a></li>
+
+</ul>
+<h2 class="surtout donate-1" id="1">Comment poster une image ?</h2> 
+<p>Ecrire l\'adresse de l\' image entre les balises [img]http://[/img] et elle apparaitra comme par magie.
+<br /> 
+<strong>Exemple&nbsp:</strong> [img]http://www.momes.net/jouets/jouets-magie/magie-cape-sorcier.jpg[/img]</p> 
+ 
+<h2 class="surtout donate-2" id="2">Peut-on redimensionner une image ?</h2> 
+<p> 
+Oui en l\'hébergeant gratuitement sur un service dédié extérieur en ligne genre 
+<strong><a href="http://imageshack.us/">Imageshack</a></strong> <em>(Ceci est un exemple, en aucun cas une pub)</em>.
+</p> 
+ 
+<h2 class="surtout donate-1" id="3">Peut-on éditer une image (La modifier une fois qu\'on l\'a posté) ?</h2> 
+<p>Le Grand Jacques l\'a dit mieux que nous : "Il faut éditer, tout peut s\'éditer qui s\'édite déjà..." 
+<br /> 
+En clair : oui bien sûr. Il suffit de recopier la nouvelle adresse de la nouvelle image.
+</p> 
+ 
+<h2 class="surtout donate-2" id="4">Comment poster une vidéo ?</h2> 
+<p> 
+Ecrire l\'adresse de la video et elle apparaitra comme par enchantement (<strong>ATTENTION SEULEMENT</strong> 
+<a href="http://www.youtube.com">Youtube</a>, <a href="http://www.dailymotion.com">Dailymotion</a> et <a href="http://www.vimeo.com">Vimeo</a> 
+pour l\'instant)
+<br /> 
+<strong>Exemple&nbsp;:</strong> http://www.youtube.com/watch?v=0rG36zItZuM</p> 
+</p> 
+ 
+<h2 class="surtout donate-1" id="5">Comment fonctionne la radio ?</h2> 
+<p> 
+Vous cliquez sur l\'image et une fenêtre apparaît . Appuyez sur <strong>PLAY</strong> et tous les mp3 postés sur le forum sont joués dans 
+l\'ordre ou de manière aléatoire, c\'est vous qui choisissez.
+<br /> Comme tout le reste, le contenu est participatif et c\'est vous (si vous le désirez) qui faites les programmes. Yo DJ TOI, fais péter le son !
+A noter que les noms des morceaux sont tels qu\'ils sont tagués sur le mp3 : pas de bras, pas de chocolat...
+</p> 
+ 
+<h2 class="surtout donate-2" id="6">Qu\'est-ce que le whisper ? (peut-on envoyer des messages privés aux membres du forum ?)</h2> 
+<p>Le mode whisper <em> (rectangle rose)</em> 
+permet d\'envoyer un message privé à un membre du forum, il faut <strong>impérativement</strong>écrire son nom dans la case whisper 
+sinon le message apparait au vu et au su de tous ! Mais dans ce cas on peut éditer son message, écrire le nom du destinataire, sauvegarder 
+le changement et tout rentre dans l\'ordre. A noter si on connaît juste le début du pseudo du destinataire, le reste s\'affiche tout seul. 
+<strong>Attention à l\'orthographe !</strong> 
+On ne peut pas seulement répondre en whisper, on peut aussi démarrer une conversation privée du moment qu\'on écrit 
+le nom du destinataire dans la fameuse case rose et qu\'on met un sujet de message.
+</p> 
+ 
+<h2 class="surtout donate-1" id="7">Peut-on changer son message si on a fait une faute, si on a changer d\'avis par exemple ?</h2> 
+<p> 
+Oui en cliquant sur <strong>EDIT</strong> (à droite). On réécrit ce que l\'on veut et ensuite on clique sur <strong>SAVE CHANGES</strong> 
+ et hop ! On peut toujours annuler en cliquant sur <strong>CANCEL</strong> si on veut...
+</p> 
+ 
+<h2 class="surtout donate-2" id="8">Peut-on faire disparaitre son message ?</h2> 
+<p> 
+Oui de la même manière en l\'éditant et en le transformant en whisper (à soi-même si on veut vraiment que plus personne ne le voit).
+<strong>A utiliser uniquement en dernier recours !</strong> 
+Attention aux résultats absurdes : conversations à trous, etc...
+On peut aussi gentiment demander à ce qu\'il soit effacé.
+</p> 
+ 
+<h2 class="surtout donate-1" id="9">Peut on citer le message de quelqu\'un d\'autre pour lui répondre ou quand on veut répondre à plusieurs personnes à la fois ?</h2> 
+<p>Meuh oui, bien sûr ! 
+On sélectionne d\'abord avec la souris la partie à citer puis on appuie sur <strong>QUOTE</strong> 
+ et le tour est joué. On peut aussi juste cliquer sur <strong>QUOTE</strong> pour citer l\'ensemble.
+</p> 
+ 
+<h2 class="surtout donate-2" id="10">Que faire quand on veut citer quelque chose qui est sur une page avant dans le même TOPIC?</h2> 
+<p>Là il faut bidouiller un peu mais c\'est possible ! On peut copier ça
+[quote][cite]Posted By: LE NOM DE LA PERSONNE QUE VOUS VOULEZ CITER[/cite]LA CITATION[/quote]
+et coller  ce qu\'il faut où il faut...
+</p> 
+ 
+<h2 class="surtout donate-1" id="11">Comment écrit on gros ou petit ?</h2> 
+<p> 
+Petit : [size=7]CE QU\'ON VEUT ECRIRE[/size] Gros [size=15]CE QU\'ON VEUT ECRIRE[/size] ce sont des exemples, on peut bien sûr mettre le chiffre qu\'on veut !
+</p> 
+ 
+<h2 class="surtout donate-2" id="12">Que signifient les petits carrés avec B I U etc ? </h2> 
+<p> 
+Ecrire en gras - Ecrire en italique - Souligner - Barrer - Aligner à gauche - Centrer - Aligner à droite - Taper du code - Choisir sa couleur de lettres - Changer de typo - 12 & 13 faire une liste - Citer - Ecrire 1er  par exemple avec er en bas - Ecrire 1er par exemple avec er en haut - TRES IMPORTANT sert à changer le nom d\'un lien : une fois qu\'on a cliquer on colle l\'adresse du lien qu\'on veut puis le nom qu\'on veut lui donner - Ecrire une adresse email
+</p> 
+ 
+<h2 class="surtout donate-1" id="13">Comment proposer un événement ?</h2> 
+<p>Aller dans <strong>AGENDA</strong>,  puis cliquer sur "proposer un évènement"  (c\'est dingue non ?). 
+Mettre un sujet (GROUPE/LIEU/DATE est un bon combo - si vous ne mettez pas la date elle n\'apparaîtra pas automatiquement dans le titre et ce sera sujet de controverses : oui mais c\'est quand ? gnagnaga) mettre la date aussi dans la case <strong>CHOOSE DATE</strong>, mettre la ville et le cas échéant le pays.
+Puis taper votre petit laïus ou recopier la prose officielle. L\'image d\'un fly est un petit plus qui ravit nos yeux (ou pas) et parfois complète l\'info !
+Vous pouvez ausi vous abonner au <strong>RSS</strong> (recevoir les news dès qu\'elles arrivent) en cliquant sur <strong>RSS</strong> à gauche. Trop belle la vie.
+<p> 
+ 
+<h2 class="surtout donate-2" id="14">Qu\'est-ce que ŒIL et INTROSPECTION ?</h2> 
+<p> 
+Dans INTROSPECTION une image s\'affiche aléatoirement  parmi toutes les images postées sur le forum, dès que vous rafraichissez votre écran ou changez de page. En cliquant sur cette image vous allez dans le topic d\'où elle vient.
+Dans ŒIL vous avez une mini gallerie avec une sélection aléatoire de 9 images qui vous emmène aussi au topic d\'origine si vous cliquez. Vous pouvez rafraichir et avoir 9 autres images. Surprises !
+</p> 
+ 
+<h2 class="surtout donate-1" id="15">Qu\'est ce que RELEASES ?</h2> 
+<p>Les <strong>RELEASES</strong> 
+ sont les sorties de disques virtuelles ou pas et les <strong>MIXES</strong> postés sur le forum. La section <strong>RELEASES</strong> tout comme le reste du forum est participative, ce sont les membres qui la nourissent. Pour que votre post apparaîsse dans RELEASES pensez à cocher C\'EST UNE RELEASE / UN MIX puis si c\'est un mix à cocher la case C\'EST UN MIX. Comme ça,les vaches seront bien gardées !
+Vous pouvez n\'afficher que les mixes si vous le souhaitez. Vous pouvez aussi vous abonner au podcast des <strong>MIXES</strong>. C\'est pas beau ça ?
+</p> 
+ 
+<h2 class="surtout donate-2" id="16">Comment proposer UNE RELEASE/UN MIX ?</h2> 
+<p> 
+Vous cliquez sur LANCER UNE DISCUSSION, vous choisissez votre catégorie, vous écrivez un sujet (TITRE/LABEL/DATE DE SORTIE semble judicieux mais c\'est vous qui voyez).
+Lien pour écouter ou mp3 directs sont du meilleur gout ! Image de pochette, infos, marketing indécent, mensonges éhontés, basse auto-flâterie : c\'est à vous !!
+</p> 
+ 
+<h2 class="surtout donate-1" id="17">Qu\'est-ce que TVi ?</h2> 
+<p> 
+<strong>TVi</strong> est une chose merveilleuse créée par <strong> <a href="http://www.glafouk.com">GLAFOUK</a></strong>,
+un des bons génies qui veillent sur ce forum.
+</p> 
+ 
+<h2 class="surtout donate-2" id="18">Oui mais concrètement TVi c\'est quoi ?</h2> 
+<p> 
+C\'est une "télé" qui diffuse aléatoirement TOUS les clips postés sur le forum DU MOMENT QU\'ILS SONT EN YOUTUBE et ce exclusivement pour des raisons techniques, bien sûr !
+Donc vous l\'avez compris : en postant des clips, des videos, des trucs pas nets qui bougent DU MOMENT QU\'ILS SONT EN YOUTUBE, vous participez aux programmes de TVi.
+Attention avant d\'allumer cette chaine, vérifiez bien que vous n\'avez rien d\'urgent à faire dans les heures qui suivent.
+</p> 
+ 
+ 
+<h2 class="surtout donate-1" id="19">Comment peut on chercher un topic, un commentaire, un membre ?</h2> 
+<p> 
+Sans <strong><a href="http://www.musiques-incongrues.net/forum/search/">SEARCH</a></strong>. Mais attention à l\'orthographe, la machine est très stricte !
+</p> 
+ 
+<h2 class="surtout donate-2" id="20">Peut on faire un don ?</h2> 
+<p> 
+Non, on DOIT faire un don ! Dans DONS justement. Vous faites comme vous voulez bien sûr mais si vous êtes là tous les jours, allez donc lire pourquoi faire un don dans DONS.
+Et c\'est sûrement bientôt Noël, ou l\'anniversaire du serveur !
+</p> 
+ 
+<h2 class="surtout donate-1" id="21">Qu\'est-ce qu\'un troll, que faire en cas de trollisme ou trollage ou trollahaine ?</H2> 
+<p>Sur un forum un troll est quelqu\'un qui cherche la polémique pour la polémique par tous les moyens, y compris l\'insulte éventuellement.
+Pour qu\'un Troll disparaisse : SURTOUT NE PAS LE NOURRIR OU ARRETER DE LE FAIRE c\'est à dire surtout l\'ignorer, ne pas lui répondre.
+Si vous mordez à l\'appat c\'est fichu. Si ça devient vraiment trop grave un Monsieur très sévère et très puissant interviendra.
+</p> 
+ 
+<h2 class="surtout donate-1" id="22">Qu\'est-ce qu\'un TOPIC (ou THREAD ou FIL DE DISCUSSION) ?</h2> 
+<p> 
+Ce n\'est ni plus ni moins qu\'un sujet de discusssion lancé par vous ou quelqu\'un d\'autre. Parfois il meurt à la naissance, parfois il grossit à vu d\'oeil car tout le monde le nourrit.
+</p> 
+ 
+<h2 class="surtout donate-2" id="23">Comment puis-je mettre mon topic dans Et Surtout ?</h2> 
+<p>Désolé petit Padawan, seuls les administrateurs et les patrons de catégories le peuvent.</p> 
+ 
+<h2 class="surtout donate-1" id="24">Comment nous contacter ?</h2> 
+<p>En cliquant sur <strong><a href="mailto:contact (CHEZ) musiques-incongrues (POINT) net">NOUS CONTACTER</a></strong> 
+ en haut à droite. Trop délire que tu l\'avais pas vu !
+</p> 
+ 
+<h2 class="surtout donate-2" id="25">Pourquoi tout ce qui est à faire n\'est pas fait en 5 minutes, pourquoi on ne me répond pas ?</h2> 
+<p>Du calme, le jeune, tout le monde est bénévole ici et a une vraie vie à côté. Tout viendra en son temps.</p> 
+ 
+</div>', 'custom' => 1, 'attribs' => '', 'roles' => array(0 => 4), 'hidden' => '')); ?>
