14348981eebdda41a55e2889272a8862cdc142dc
Tristan RIVOALLAN <tristan@cafe.com>
2010-11-12 02:46:05 +0100

    [shop] Implemented form ui controls & created symfony module.

--- forum/conf/extensions.php
+++ forum/conf/extensions.php
@@ -23,10 +23,9 @@ include($Configuration['EXTENSIONS_PATH']."vanilla-alaune/default.php");
 include($Configuration['EXTENSIONS_PATH']."vanilla-visuels/default.php");
 include($Configuration['EXTENSIONS_PATH']."PageMng/default.php");
 include($Configuration['EXTENSIONS_PATH']."vanilla-player/default.php");
-include($Configuration['EXTENSIONS_PATH']."vanilla-shop/default.php");
 include($Configuration['EXTENSIONS_PATH']."comments_permalinks/default.php");
 include($Configuration['EXTENSIONS_PATH']."MembersList/default.php");
 include($Configuration['EXTENSIONS_PATH']."SubCategories/default.php");
 include($Configuration['EXTENSIONS_PATH']."vanilla-symfony/default.php");
 include($Configuration['EXTENSIONS_PATH']."DiscussionFilters/default.php");
-?>
+include($Configuration['EXTENSIONS_PATH']."vanilla-shop/default.php");
