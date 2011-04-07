f383657dc2c7e7507815f818db97c1b521c2d10f
Tristan Rivoallan <tristan@rivoallan.net>
2011-04-07 21:14:08 +0200

    [ext/MiExpandContents] Handle more file types.

--- forum/extensions/MiExpandContents/js/behaviors.js
+++ forum/extensions/MiExpandContents/js/behaviors.js
@@ -2,8 +2,25 @@ jQuery(document).ready(function($) {
 	$('a[href$=".jpg"]').each(function() {
 		$(this).after($('<img src="'+ $(this).attr('href') +'" />')).remove();
 	});
+	$('a[href$=".jpeg"]').each(function() {
+		$(this).after($('<img src="'+ $(this).attr('href') +'" />')).remove();
+	});
 	$('a[href$=".gif"]').each(function() {
 		$(this).after($('<img src="'+ $(this).attr('href') +'" />')).remove();
 	});
-
+	$('a[href$=".png"]').each(function() {
+		$(this).after($('<img src="'+ $(this).attr('href') +'" />')).remove();
+	});
+	$('a[href$=".tiff"]').each(function() {
+		$(this).after($('<img src="'+ $(this).attr('href') +'" />')).remove();
+	});
+	$('a[href$=".tif"]').each(function() {
+		$(this).after($('<img src="'+ $(this).attr('href') +'" />')).remove();
+	});
+	$('a[href$=".pict"]').each(function() {
+		$(this).after($('<img src="'+ $(this).attr('href') +'" />')).remove();
+	});
+	$('a[href$=".bmp"]').each(function() {
+		$(this).after($('<img src="'+ $(this).attr('href') +'" />')).remove();
+	});
 });
