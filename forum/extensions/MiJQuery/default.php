ed77036b849cf3f87dfe015458bd918a31264df8
Tristan Rivoallan <tristan@rivoallan.net>
2011-04-27 13:57:31 +0200

    [ext/MiJQuery] Added jQuery Tools.

--- forum/extensions/MiJQuery/default.php
+++ forum/extensions/MiJQuery/default.php
@@ -1,2 +1,6 @@
 <?php
+// jQuery - http://www.jquery.com
 $Head->AddScript('http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js');
+
+// jQuery Tools - http://flowplayer.org/tools/ 
+$Head->AddScript('http://cdn.jquerytools.org/1.2.5/all/jquery.tools.min.js');
