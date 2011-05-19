9dbe0b79c6d9dfe3effb7a964d30a62021d7187d
Tristan Rivoallan <tristan@rivoallan.net>
2011-05-19 23:51:20 +0200

    [ext/MiJQuery] Jquery is no longer required from Google CDN

--- forum/extensions/MiJQuery/default.php
+++ forum/extensions/MiJQuery/default.php
@@ -3,10 +3,7 @@
 $extensionName = basename(dirname(__FILE__));
 
 // jQuery - http://www.jquery.com
-$Head->AddScript('http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js');
-
-// jQuery Tools - http://flowplayer.org/tools/ 
-$Head->AddScript('http://cdn.jquerytools.org/1.2.5/all/jquery.tools.min.js');
+$Head->AddScript(sprintf('extensions/%s/js/jquery/jquery-1.5.2.min.js', $extensionName));
 
 // jQuery Thumbs - http://joanpiedra.com/jquery/thumbs/
 $Head->AddScript(sprintf('extensions/%s/js/jquery/thumbs/jquery.thumbs.js', $extensionName));
