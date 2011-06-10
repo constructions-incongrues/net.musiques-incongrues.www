f3e44b6a556e9191a803e3b0bdb9ef982f872a50
Tristan Rivoallan <tristan@rivoallan.net>
2011-06-10 05:00:05 +0200

    [page player] Restricted beta test

--- forum/extensions/MiPagePlayer/default.php
+++ forum/extensions/MiPagePlayer/default.php
@@ -8,14 +8,16 @@
  Author Url: http://github.com/trivoallan
  */
 
-// Activate extension when view a discussion
-if ($Context->SelfUrl == 'comments.php') {
-	$Head->AddScript('extensions/MiPagePlayer/js/MiPagePlayer.playlist.js');
-	$Head->AddScript('extensions/MiPagePlayer/js/MiPagePlayer.behaviors.js?'.time());
-	$Head->AddStyleSheet('extensions/MiPagePlayer/css/MiPagePlayer.main.css?'.time());
-	$Context->AddToDelegate('CommentGrid', 'PostRender', 'MiPagePlayer_PostRenderCommentFoot');
-}
-
-function MiPagePlayer_PostRenderCommentFoot(CommentGrid $commentGrid) {
-	include(dirname(__FILE__).'/templates/page-player.php');
+if ($Context->Configuration['FEATURES']['pagePlayer']['restricted'] && in_array($Context->Session->UserID, $Context->Configuration['FEATURES']['pagePlayer']['uids'])) {
+	// Activate extension when view a discussion
+	if ($Context->SelfUrl == 'comments.php') {
+		$Head->AddScript('extensions/MiPagePlayer/js/MiPagePlayer.playlist.js');
+		$Head->AddScript('extensions/MiPagePlayer/js/MiPagePlayer.behaviors.js?'.time());
+		$Head->AddStyleSheet('extensions/MiPagePlayer/css/MiPagePlayer.main.css?'.time());
+		$Context->AddToDelegate('CommentGrid', 'PostRender', 'MiPagePlayer_PostRenderCommentFoot');
+	}
+	
+	function MiPagePlayer_PostRenderCommentFoot(CommentGrid $commentGrid) {
+		include(dirname(__FILE__).'/templates/page-player.php');
+	}
 }
