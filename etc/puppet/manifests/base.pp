e02450958f68a6b42ad3e8298cb507a07ccd9b72
Tristan Rivoallan <tristan@rivoallan.net>
2012-12-31 17:22:11 +0100

    [vagrant] working vagrant VM

---
+++ etc/puppet/manifests/base.pp
@@ -0,0 +1,54 @@
+class com::thibaulthuertas::www {
+  exec { 'apt-get update':
+    command => '/usr/bin/apt-get update'
+  }
+  Exec["apt-get update"] -> Package <| |>
+
+  exec { 'a2enmod rewrite':
+    command => '/usr/sbin/a2enmod rewrite',
+    require => Package['apache2'],
+    notify => Service['apache2']
+  }
+
+  exec { 'mysql load dumps': 
+    command => '/usr/bin/mysql -uroot -e "create database main default charset utf8 collate utf8_general_ci; create database net_musiquesincongrues_www_asaph default charset utf8 collate utf8_general_ci" && /usr/bin/mysql -uroot main < /vagrant/data/sql/vanilla.sql && /usr/bin/mysql -uroot main < /vagrant/data/sql/asaph.sql',
+    require => Package['mysql-server']
+  }
+
+  package { ["ant", "apache2", "libapache2-mod-php5", "php5-cli", "php5-curl", "php5-mysqlnd", "php-apc", "php5-intl", "vim-tiny", "mysql-server"]:
+    ensure => present,
+  }
+
+  service { ["mysql"]:
+    ensure => running,
+    require => Package["mysql-server"],
+  }
+  
+  service { ["apache2"]:
+    ensure => running,
+    require => Package["apache2"],
+  }
+
+  file { '/etc/apache2/sites-available/default':
+    ensure => present,
+    source => "puppet:///vagrant_puppet_files/etc/apache2/sites-available/default",
+    notify => Service['apache2'],
+    require => Package["apache2"],
+  }
+
+  file { '/etc/apache2/envvars':
+    ensure => present,
+    source => "puppet:///vagrant_puppet_files/etc/apache2/envvars",
+    require => Package["apache2"],
+  }
+
+  file { '/etc/php5/conf.d/9900-local.ini':
+    ensure => present,
+    source => "puppet:///vagrant_puppet_files/etc/php5/conf.d/9900-local.ini",
+    require => Package["apache2"],
+    notify => Service['apache2'],
+  }
+
+}
+
+include com::thibaulthuertas::www
