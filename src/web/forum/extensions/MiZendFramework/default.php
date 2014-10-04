0f22a61d4cbfc7701ddbd9a6cf7c64002b112f0c
Tristan Rivoallan <tristan@rivoallan.net>
2014-10-04 19:12:00 +0200

    [zend] passage en mode Composer

--- src/web/forum/extensions/MiZendFramework/default.php
+++ src/web/forum/extensions/MiZendFramework/default.php
@@ -9,6 +9,6 @@
  */
 
 // Setup Zend framework autoloading
-set_include_path(dirname(__FILE__).'/../../../lib/vendor/ZendFramework-1.11.5-minimal/library/'.PATH_SEPARATOR.get_include_path());
-require_once(dirname(__FILE__).'/../../../lib/vendor/ZendFramework-1.11.5-minimal/library/Zend/Loader/Autoloader.php');
+set_include_path(dirname(__FILE__).'/../../../../../vendor/zendframework/zendframework1/library/'.PATH_SEPARATOR.get_include_path());
+require_once(dirname(__FILE__).'/../../../../../vendor/zendframework/zendframework1/library/Zend/Loader/Autoloader.php');
 Zend_Loader_Autoloader::getInstance();
