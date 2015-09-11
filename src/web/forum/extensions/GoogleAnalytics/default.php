527d2e5995aece5814b7b707c3681343281792ca
Tristan Rivoallan <tristan.rivoallan@aramisauto.com>
2015-09-11 15:21:13 +0200

    [google analytics] passage à universal analytics

--- src/web/forum/extensions/GoogleAnalytics/default.php
+++ src/web/forum/extensions/GoogleAnalytics/default.php
@@ -12,26 +12,29 @@ Author Url: http://lussumo.com/community/discussion/3507/
 $Configuration['GoogleAnalyticsAccountNumber'] = 'UA-673133-2';
 
 //add an array of pages to not analyse
-if ( !in_array($Context->SelfUrl, array('settings.php')) && isset($Head) )
+if (!in_array($Context->SelfUrl, array('settings.php')) && isset($Head))
 {
-	class AddGoogleAnalytics extends Control
-	{
-		function Render()
-		{
-			global $Configuration;
-			echo '
-            <script type="text/javascript" src="http://www.google-analytics.com/urchin.js"></script>
-            <script type="text/javascript">
-            
-            // <![CDATA[
-            _uacct="'.$Configuration['GoogleAnalyticsAccountNumber'].'";
-            urchinTracker();
-            // ]]>
+    class AddGoogleAnalytics extends Control
+    {
+        function Render()
+        {
+            global $Configuration;
+            echo '
+            <!-- Google Analytics -->
+            <script>
+            (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,"script","//www.google-analytics.com/analytics.js","ga");
+
+            ga("create", "'.$Configuration['GoogleAnalyticsAccountNumber'].'", "auto");
+            ga("send", "pageview");
+
             </script>
+            <!-- End Google Analytics -->
 ';
-		}
-	}
-	$AddGoogleAnalytics = $Context->ObjectFactory->NewContextObject($Context, "AddGoogleAnalytics");
-	$Page->AddControl("Page_Render", $AddGoogleAnalytics, $Configuration["CONTROL_POSITION_FOOT"]);
+        }
+    }
+    $AddGoogleAnalytics = $Context->ObjectFactory->NewContextObject($Context, "AddGoogleAnalytics");
+    $Page->AddControl("Page_Render", $AddGoogleAnalytics, $Configuration["CONTROL_POSITION_FOOT"]);
 }
-?>
