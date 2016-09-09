25eea3eaf7a40c7e86589e886ab0da6b2794cd4e
Tristan Rivoallan <tristan@rivoallan.net>
2016-09-09 11:51:02 +0200

    [forum] fix pour mysql 5.7

--- src/web/forum/conf/extensions.php
+++ src/web/forum/conf/extensions.php
@@ -2,6 +2,7 @@
 // Make sure this file was not accessed directly and prevent register_globals configuration array attack
 if (!defined('IN_VANILLA')) exit();
 // Enabled Extensions
+include($Configuration['EXTENSIONS_PATH']."MiCompat/default.php");
 include($Configuration['EXTENSIONS_PATH']."MiZendFramework/default.php");
 include($Configuration['EXTENSIONS_PATH']."MiJQuery/default.php");
 include($Configuration['EXTENSIONS_PATH']."MiVanillaMiner/default.php");
---
+++ src/web/forum/extensions/MiCompat/default.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ Extension Name: MiCompat
+ Extension Url: https://github.com/contructions-incongrues
+ Description: Workaround for compatibility with newer platforms
+ Version: 0.1
+ Author: Tristan Rivoallan <tristan@rivoallan.net>
+ Author Url: http://github.com/trivoallan
+ */
+$Context->Database->execute('SET sql_mode = ""');
